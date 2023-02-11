8a788b54d00d7500fc3f2ddb00885f0a19b7ecbf
Cynthia Uche <cynthianuchepro@gmail.com>
2023-02-10 17:42:24 -0800

    Fix Pycodestyle fail

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -8,6 +8,7 @@ class TestUser(unittest.TestCase):
     def test_no_args_instantiation(self):
         u1 = User()
         self.assertIsInstance(u1, User)
+
     def test_User_inherits_from_BaseModel(self):
         u1 = User()
         self.assertIsInstance(u1, BaseModel)
