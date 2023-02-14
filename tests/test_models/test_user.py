62545a1d9c999050407618aa04e892173e19fbfb
Cynthia Uche <cynthianuchepro@gmail.com>
2023-02-14 01:06:59 -0800

    Add tests for User

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ Test Module for testing class User """
 import unittest
+from models import storage
+from datetime import datetime
 from models.base_model import BaseModel
 from models.user import User
 
@@ -15,6 +17,30 @@ class TestUser(unittest.TestCase):
     def test_User_inherits_from_BaseModel(self):
         """ Tests if class user inherited from class BaseModel """
         self.assertTrue(issubclass(User, BaseModel))
+
+    def test_new_instance_stored_in_objects(self):
+        self.assertIn(User(), storage.all().values())
+
+    def test_id_is_public_str(self):
+        self.assertEqual(str, type(User().id))
+
+    def test_created_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().created_at))
+
+    def test_updated_at_is_public_datetime(self):
+        self.assertEqual(datetime, type(User().updated_at))
+
+    def test_email_is_public_str(self):
+        self.assertEqual(str, type(User.email))
+
+    def test_password_is_public_str(self):
+        self.assertEqual(str, type(User.password))
+
+    def test_first_name_is_public_str(self):
+        self.assertEqual(str, type(User.first_name))
+
+    def test_last_name_is_public_str(self):
+        self.assertEqual(str, type(User.last_name))
 
 
 if __name__ == "__main__":
